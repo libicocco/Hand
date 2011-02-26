94fe907fb2f14dd957d3a8b0faff00dc948d8d63
Javier Romero <jrgn@kth.se>
2011-02-26 23:13:54 +0100

    Hogs are inserted in the DB, and  poses are interpolated in renderHand

--- scene/renderhand.cc
+++ scene/renderhand.cc
@@ -41,16 +41,17 @@
 #include "cDB.h"
 #include "cDBelement.h"
 
+static const unsigned gNFrames(5);
 static const buola::C3DVector lHandZero(0.31,-0.57,0.02);
 static const buola::C3DVector lObjZero(0,0,0);
 static const double gCamDistance(0.3);
-static const unsigned gNumViews(10);
+static const unsigned gNumViews(1000);
 
 using namespace buola;
 
 int main(int pNArg,char **pArgs)
 {
-  const std::vector<std::string> &lPosePathV=CProgramOption::GetArgs();
+//   const std::vector<std::string> &lPosePathV=CProgramOption::GetArgs();
   
   srand(time(NULL));
   buola_init(pNArg,pArgs);
@@ -86,126 +87,136 @@ int main(int pNArg,char **pArgs)
     }
   }
   
-//   float lYPR[gNumViews*3]={0,0,0,0,0,M_PI/2,0,M_PI/2,0,0,M_PI/2,M_PI/2,M_PI/2,0,0,M_PI/2,0,M_PI/2,M_PI/2,M_PI/2,0,M_PI/2,M_PI/2,M_PI/2};
-  
-//   float *lYPR=new float[gNumViews*3];
-//   static const float gMaxRadious=static_cast<float>(RAND_MAX/2);
-//   for(int i=0;i<gNumViews;)
-//   {
-//     float x=static_cast<float>(rand())-gMaxRadious;
-//     float y=static_cast<float>(rand())-gMaxRadious;
-//     float z=static_cast<float>(rand())-gMaxRadious;
-//     if(x*x+y*y+z*z <= gMaxRadious*gMaxRadious && (x!=0 && y!=0 && z!=0))
-//     {
-//       lYPR[i*3]=atan2(y,x); // yaw
-//       lYPR[i*3+1]=atan2(z,sqrt(x*x+y*y)); //pitch
-//       lYPR[i*3+2]=(static_cast<float>(rand())-gMaxRadious)*(2*M_PI/static_cast<float>(RAND_MAX)); // roll
-//       ++i;
-//     }
-//   }
-  
   try
   {
     std::ofstream lHOGFS;
     CDB *lDB=(gDBPathOption.IsSet())?new CDB(gDBPathOption.GetValue()):NULL;
     CDB lDBtaxonomy("taxonomy.db");
+//     CDB lDBtaxonomy("hands.db");
+    std::vector<float> lFeature;
     if(gHOGPathOption.IsSet())
       lHOGFS.open(gHOGPathOption.GetValue().c_str());
     
-    for(int p=0;p<lPosePathV.size();++p)
+    CDBelement lDBelemRest=lDBtaxonomy.query(0);
+    tFullPoseV lRestPose;
+    lDBelemRest.getFullPose(lRestPose);
+    Hog<float> lHog;
+    unsigned lFeatSize=lHog.getFeatSize();
+    float *lFeatureA=new float[lFeatSize*31*gNFrames*gNumViews];
+    
+//     for(int p=0;p<lPosePathV.size();++p)
+    for(int p=0;p<31;++p)
     {
-      CDBelement lDBelem=lDBtaxonomy.query(p);
-//       std::cout << lDBelem << std::endl;
-//       loadPose(lPosePathV[p],lSkeleton,lHandTransf,lObjTransf,lObjectPath,lCam2PalmRArray);
-      loadPose(lDBelem,lSkeleton,lHandTransf,lObjTransf,lObjectPath,lCam2PalmRArray);
-//       CDBelement lDBelem(lPosePathV[p],p);
-//       lDBtaxonomy.insertElement(lDBelem);
-      
-      scene::PGeode lGeode=buola::scene::CGeode::Import(lObjectPath.c_str(),0.1);
+      CDBelement lDBelem=lDBtaxonomy.query(p+1); // skipping rest pose
+      //       std::cout << lDBelem << std::endl;
+      //       loadPose(lPosePathV[p],lSkeleton,lHandTransf,lObjTransf,lObjectPath,lCam2PalmRArray);
+      //       CDBelement lDBelem(lPosePathV[p],p);
+      //       lDBtaxonomy.insertElement(lDBelem);
       
       scene::PPerspectiveCamera lCamera=new scene::CPerspectiveCamera;
       lCamera->SetClipping(0.01,200);
-      CSaveButton lButton(lSkeleton,lHandTransf,lObjTransf,lCamera,lObjectPath,NULL);
+//       CSaveButton lButton(lSkeleton,lHandTransf,lObjTransf,lCamera,lObjectPath,NULL);
       
       scene::PScene lScene=new scene::CScene;
       
-      lScene->GetWorld()->AddChild(lHandTransf);
-      lHandTransf->AddChild(lSkeleton.GetSkeleton()->GetRoot()->GetTransform());
-      lScene->AddObject(lSkeleton.GetSkeleton());
-      
-      lGeode->AttachTo(lObjTransf);
-      lScene->GetWorld()->AddChild(lObjTransf);
-      
       scene::CImageRenderer lRenderer;
-      lRenderer.SetScene(lScene);
       lRenderer.SetClearColor(buola::CColor(0,0,0));
       buola::img::CImage_rgb8 lImage({400,400});
       
-      Hog<float> lHog;
-      for(int i=0;i<gNumViews;++i)
+      tFullPoseV lFullPose;
+      lDBelem.getFullPose(lFullPose);
+      std::stringstream lPoseFolder;
+      lPoseFolder << std::setfill('0');
+      lPoseFolder << "out/" << std::setw(3) << p;
+      fsystem::create_directory(lPoseFolder.str());
+      for(int f=0;f<gNFrames;++f)
       {
-        //std::cout << i << std::endl;
-        //         lCamera->LookAt(C3DVector(0,0,0),C3DRotation(lYPR[i*3],lYPR[i*3+1],lYPR[i*3+2]),0.25);
+        tFullPoseV lFullPoseInterp=((gNFrames-(f+1))*lRestPose+(f+1)*lFullPose)/gNFrames;
+        lDBelem.setFullPose(lFullPoseInterp);
+        loadPose(lDBelem,lSkeleton,lHandTransf,lObjTransf,lObjectPath,lCam2PalmRArray);
         
-        C3DVector lAt(0,0,0);
-        C3DVector lFrom(lXYZ[i].first);
-        C3DVector lUp(lXYZ[i].second);
-        lCamera->LookAt(lAt,lFrom,lUp);
+        lScene->GetWorld()->AddChild(lHandTransf);
+        lHandTransf->AddChild(lSkeleton.GetSkeleton()->GetRoot()->GetTransform());
+        lScene->AddObject(lSkeleton.GetSkeleton());
         
-        lRenderer.SetCamera(lCamera);
-        lRenderer.GetImage(mview(lImage));
-        std::stringstream lPath;
-        lPath << std::setfill('0');
-        lPath << "out/test" << std::setw(3) << p << "_" << std::setw(3) << i << ".pgm";
-        //         lPath << "out/" << std::setw(3) << p << "_" << std::setw(3) << int(rad2deg(lYPR[i*3])) << "_" << std::setw(3) << int(rad2deg(lYPR[i*3+1])) << "_" << std::setw(3) << int(rad2deg(lYPR[i*3+2])) << ".pgm";
-        buola::save_pgm(view(lImage),lPath.str());
-        lPath.seekp(static_cast<long>(lPath.tellp())-4);
-        lPath << ".txt";
-        lButton.saveURL(lPath.str());
+        scene::PGeode lGeode=buola::scene::CGeode::Import(lObjectPath.c_str(),0.1); // why if I put this outside the loop it does weird things?
+        lGeode->AttachTo(lObjTransf);
+        lScene->GetWorld()->AddChild(lObjTransf);
         
-        if(gHOGPathOption.IsSet())
+        lRenderer.SetScene(lScene);
+      
+        for(int i=0;i<gNumViews;++i)
         {
-          cv::Mat lImageCV=cv::Mat(buola::cvi::ipl_wrap(view(lImage)),false);
-          cv::Mat lGrayIm(lImageCV.size(),CV_8UC1);
-          cv::Mat lImageCV32F(lImageCV.size(),CV_32FC3);
-          lImageCV.convertTo(lImageCV32F,CV_32FC3);
-          cv::cvtColor(lImageCV,lGrayIm,CV_BGR2GRAY);
-          cv::threshold(lGrayIm,lGrayIm,1,255,cv::THRESH_BINARY);
+          //std::cout << i << std::endl;
+          //         lCamera->LookAt(C3DVector(0,0,0),C3DRotation(lYPR[i*3],lYPR[i*3+1],lYPR[i*3+2]),0.25);
           
-          std::vector<cv::Point> lAllContours;
+          C3DVector lAt(0,0,0);
+          C3DVector lFrom(lXYZ[i].first);
+          C3DVector lUp(lXYZ[i].second);
+          lCamera->LookAt(lAt,lFrom,lUp);
+          
+          lRenderer.SetCamera(lCamera);
+          lRenderer.GetImage(mview(lImage));
+          std::stringstream lPoseName;
+          lPoseName << std::setfill('0');
+          lPoseName << std::setw(3) << f << "_" << std::setw(3) << i << ".pgm";
+          fsystem::path lPosePath(lPoseFolder.str());
+          lPosePath/=lPoseName.str();
+          //         lPath << "out/" << std::setw(3) << p << "_" << std::setw(3) << int(rad2deg(lYPR[i*3])) << "_" << std::setw(3) << int(rad2deg(lYPR[i*3+1])) << "_" << std::setw(3) << int(rad2deg(lYPR[i*3+2])) << ".pgm";
+          buola::save_pgm(view(lImage),lPosePath.string());
+//           lPath.seekp(static_cast<long>(lPath.tellp())-4);
+//           lPath << ".txt";
+//           lButton.saveURL(lPath.str());
+          
+          if(gHOGPathOption.IsSet())
           {
-            std::vector<std::vector<cv::Point> > lContours;
-            cv::Mat lTmp = lGrayIm.clone();
-            cv::findContours(lTmp, lContours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-            for(int i=0;i<lContours.size();++i)
-              lAllContours.insert(lAllContours.end(),lContours[i].begin(),lContours[i].end());
+            cv::Mat lImageCV=cv::Mat(buola::cvi::ipl_wrap(view(lImage)),false);
+            cv::Mat lGrayIm(lImageCV.size(),CV_8UC1);
+            cv::Mat lImageCV32F(lImageCV.size(),CV_32FC3);
+            lImageCV.convertTo(lImageCV32F,CV_32FC3);
+            cv::cvtColor(lImageCV,lGrayIm,CV_BGR2GRAY);
+            cv::threshold(lGrayIm,lGrayIm,1,255,cv::THRESH_BINARY);
+            
+            std::vector<cv::Point> lAllContours;
+            {
+              std::vector<std::vector<cv::Point> > lContours;
+              cv::Mat lTmp = lGrayIm.clone();
+              cv::findContours(lTmp, lContours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+              for(int i=0;i<lContours.size();++i)
+                lAllContours.insert(lAllContours.end(),lContours[i].begin(),lContours[i].end());
+            }
+            
+            cv::Rect lBBox=cv::boundingRect(cv::Mat(lAllContours));
+            
+            std::pair<cv::Mat,cv::Mat> lTstMaskCrop=std::make_pair(lImageCV32F(lBBox),lGrayIm(lBBox));
+            lFeature=lHog.compute(lTstMaskCrop,99999999);
+            std::copy(lFeature.begin(),lFeature.end(),&(lFeatureA[(p*(gNumViews*gNFrames)+f*gNumViews+i)*lFeatSize]));
+//             std::copy(lFeature.begin(),lFeature.end(),std::ostream_iterator<float>(lHOGFS," "));
+//             lHOGFS << std::endl;
           }
-          
-          cv::Rect lBBox=cv::boundingRect(cv::Mat(lAllContours));
-          
-          std::pair<cv::Mat,cv::Mat> lTstMaskCrop=std::make_pair(lImageCV32F(lBBox),lGrayIm(lBBox));
-          std::vector<float> lTstFloat=lHog.compute(lTstMaskCrop,99999999);
-          std::copy(lTstFloat.begin(),lTstFloat.end(),std::ostream_iterator<float>(lHOGFS," "));
-          lHOGFS << std::endl;
+          if(gDBPathOption.IsSet())
+          {
+            lDBelem.setCamAtFromUp(lAt,lFrom,lUp);
+            lDBelem.setIndex(p*(gNumViews*gNFrames)+f*gNumViews+i);
+            if(gHOGPathOption.IsSet())
+              lDBelem.setFeature(lFeature);
+            lDB->insertElement(lDBelem);
+          }
         }
-        if(gDBPathOption.IsSet())
-        {
-          lDBelem.setCamAtFromUp(lAt,lFrom,lUp);
-          lDBelem.setIndex(p*gNumViews+i);
-          lDB->insertElement(lDBelem);
-        }
-      }
       lObjTransf->RemoveObject(lGeode);
+      }
     }
     if(gHOGPathOption.IsSet())
+    {
+      lHOGFS.write(reinterpret_cast<char*>(lFeatureA),lFeatSize*31*gNFrames*gNumViews*sizeof(float));
       lHOGFS.close();
+    }
     if(gDBPathOption.IsSet())
     {
       lDB->finalizeStatement();
-//       lDB->prepareQuery();
-//      delete lDB;
+      //      delete lDB;
     }
-//     lDBtaxonomy.finalizeStatement();
+    delete []lFeatureA;
   }
   catch(std::exception &pE)
   {
